dc1126ae7f4d1f1ed37f23384616f1bcd9c52e97
Hamit Mizrak <hamitmizrak@gmail.com>
2024-12-30 11:32:21 +0300

    initial

---
+++ src/main/java/com/hamitmizrak/techcareer_2025_backend_1/Techcareer2025Backend1Application.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak.techcareer_2025_backend_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Techcareer2025Backend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Techcareer2025Backend1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamitmizrak/techcareer_2025_backend_1/Techcareer2025Backend1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak.techcareer_2025_backend_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Techcareer2025Backend1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
