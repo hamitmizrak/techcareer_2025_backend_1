5d1681f8dd2643ef8a3cc116acf72a479443a097
Hamit Mizrak <hamitmizrak@gmail.com>
2025-01-06 10:31:47 +0300

    ApiResultValidMessages, PersistPermanent, Config,

---
+++ src/main/java/com/hamitmizrak/techcareer_2025_backend_1/utils/FrontEnd.java
@@ -0,0 +1,7 @@
+package com.hamitmizrak.techcareer_2025_backend_1.utils;
+
+public class FrontEnd {
+    // Const
+    public static final String REACT_URL="http://localhost:3000";
+    public static final String ANGULAR_URL="http://localhost:3040";
+}
---
+++ src/main/java/com/hamitmizrak/techcareer_2025_backend_1/utils/PermaLink.java
@@ -0,0 +1,10 @@
+package com.hamitmizrak.techcareer_2025_backend_1.utils;
+
+public class PermaLink {
+    /*
+    http://localhost:4444
+    http://localhost:4444/h2-console
+    http://localhost:4444/swagger-ui/index.html
+    http://localhost:4444/actuator/health
+    */
+}
