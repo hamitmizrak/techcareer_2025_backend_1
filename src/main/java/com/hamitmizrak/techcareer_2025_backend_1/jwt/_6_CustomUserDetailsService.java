983ddfd4b97e276ae1433b20ffcfcd716907c9da
Hamit Mizrak <hamitmizrak@gmail.com>
2025-01-10 22:33:04 +0300

    CustomUserDetailService

--- src/main/java/com/hamitmizrak/techcareer_2025_backend_1/jwt/_6_CustomUserDetailsService.java
+++ src/main/java/com/hamitmizrak/techcareer_2025_backend_1/jwt/_6_CustomUserDetailsService.java
@@ -1,21 +1,24 @@
 package com.hamitmizrak.techcareer_2025_backend_1.jwt;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 // LOMBOK
 @RequiredArgsConstructor
 
+// Service
 @Service
 public class _6_CustomUserDetailsService implements UserDetailsService {
 
     // Injection
     private final _5_IUserRepository iUserRepository;
-
 
     // Kullanıcı var mı Yok mu ?  Eğer yoksa: `User not found`
     // Kulalnıcı varsa UserDetail'in içine buludğum veriyi set et
@@ -28,13 +31,23 @@ public class _6_CustomUserDetailsService implements UserDetailsService {
 
         // User kütüphanesinden şimdiki buluduğumuz _4_UserEntity verileri set et
         // import org.springframework.security.core.userdetails.User;
-        UserDetails userDetails= User.builder()
+        // Kullanıcının rolünü GrantedAuthority'ye dönüştürmek
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(userEntity.getRole().toString()));
+
+        // 1.YOL
+        UserDetails userDetails= new User(userEntity.getUsername(),"{noop}" + userEntity.getPassword(),authorities);
+
+        // 2.YOL
+        /*
+        UserDetails userDetails=User.builder()
                 .username(userEntity.getUsername())
-                //.password(userEntity.getPassword())
-                //.password("{noop}"+userEntity.getPassword())
+                .password("{noop}" + userEntity.getPassword()) // Şifreye {noop} öneki eklendi
+                .password(userEntity.getPassword())
+                .password("{noop}"+userEntity.getPassword())
                 .password("{noop}root")
                 .roles(userEntity.getRole().toString())
                 .build();
+                */
 
         return userDetails;
     } //end loadUserByUsername
